7ffab526b541dc1bd207e01d3366bb904aa6753e
AnthLima <tryout3699@gmail.com>
2022-09-18 04:07:42 -0300

    feat: Add insert new user into database, and some checks and validations for the data to be registered

--- src/app/Controllers/UserController.js
+++ src/app/Controllers/UserController.js
@@ -1,6 +1,10 @@
+const bcrypt = require('bcryptjs');
+const yup = require('yup');
+const User = require('../Models/UserSchema');
+
 class UserController {
   
-  index(req,res) {
+  index(req) {
     console.log(req.body);
   }
   
@@ -10,6 +14,53 @@ class UserController {
       error: false,
       users
     });
+  }
+
+  async store(req, res) {
+
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+      password: yup.string().required()
+    });
+
+    if(!(await schema.isValid(req.body))){
+      res.status(400).json({
+        "message": "Oops! Invalid data!",
+      })
+    }
+    
+    const userExist = await User.findOne({ email: req.body.email });
+
+    if(userExist){
+      res.status(400).json({
+        "message": "Oops! The email already exists!"
+      });
+    }
+
+    const { name, email, password } = req.body;
+
+    const data = { name, email, password };
+
+    data.password = await bcrypt.hash(data.password, 8);
+
+    await User.create(data)
+    .then(() => {
+      res.status(201).json(
+        {
+          "message":"User created successfully"
+        }
+      );
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(404).json(
+        {
+          "message":"Oops! Failed to create user",
+          "error": error 
+        }
+      );
+    })
   }
 
 }
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,8 @@ const UserController = require('./app/Controllers/UserController');
 
 const routes = new Router();
 
-routes.post('/user', UserController.index);
-routes.get('/users', UserController.show);
+
+routes.post('/user', UserController.store);
+routes.get('/user', UserController.show);
 
 module.exports = routes;
